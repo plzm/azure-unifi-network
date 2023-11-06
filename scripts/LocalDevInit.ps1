ce94e071fd16d4997721c375423db0729628cf0a
Patrick El-Azem <p@elazem.com>
2023-11-06 05:29:46 -0500

    Add local dev init script

---
+++ scripts/LocalDevInit.ps1
@@ -0,0 +1,5 @@
+. ./scripts/infra/Utility.ps1
+
+$configConstants = Get-ConfigFromFile -ConfigFilePath "./config/infra_constants.json"
+$configMain = Get-ConfigFromFile -ConfigFilePath "./config/infra_main.json"
+
