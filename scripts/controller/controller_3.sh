6d2d42150ea861ae2ef98b9ed1431b61e43d5a01
Patrick El-Azem <p@elazem.com>
2023-12-18 11:26:59 -0500

    Restore entire pipeline

--- scripts/controller/controller_3.sh
+++ scripts/controller/controller_3.sh
@@ -4,7 +4,7 @@ set -eux
 
 # Slightly modified version of the below
 # Modified December 2023 by Patrick El-Azem to tokenize VM name for pipeline use
-touch ~/11.txt
+
 # unifi_ssl_import.sh
 # UniFi Controller SSL Certificate Import Script for Unix/Linux Systems
 # by Steve Jenkins <http://www.stevejenkins.com/>
@@ -66,37 +66,37 @@ printf "\nStarting UniFi Controller SSL Import...\n"
 # Check to see whether Let's Encrypt Mode (LE_MODE) is enabled
 
 if [[ ${LE_MODE} == "YES" || ${LE_MODE} == "yes" || ${LE_MODE} == "Y" || ${LE_MODE} == "y" || ${LE_MODE} == "TRUE" || ${LE_MODE} == "true" || ${LE_MODE} == "ENABLED" || ${LE_MODE} == "enabled" || ${LE_MODE} == 1 ]] ; then
-	LE_MODE=true
-	printf "\nRunning in Let's Encrypt Mode...\n"
-	PRIV_KEY=${LE_LIVE_DIR}/${UNIFI_HOSTNAME}/privkey.pem
-	CHAIN_FILE=${LE_LIVE_DIR}/${UNIFI_HOSTNAME}/fullchain.pem
+  LE_MODE=true
+  printf "\nRunning in Let's Encrypt Mode...\n"
+  PRIV_KEY=${LE_LIVE_DIR}/${UNIFI_HOSTNAME}/privkey.pem
+  CHAIN_FILE=${LE_LIVE_DIR}/${UNIFI_HOSTNAME}/fullchain.pem
 else
-	LE_MODE=false
-	printf "\nRunning in Standard Mode...\n"
+  LE_MODE=false
+  printf "\nRunning in Standard Mode...\n"
 fi
 
 if [[ ${LE_MODE} == "true" ]]; then
-	# Check to see whether LE certificate has changed
-	printf "\nInspecting current SSL certificate...\n"
-	if md5sum -c "${LE_LIVE_DIR}/${UNIFI_HOSTNAME}/privkey.pem.md5" &>/dev/null; then
-		# MD5 remains unchanged, exit the script
-		printf "\nCertificate is unchanged, no update is necessary.\n"
-		exit 0
-	else
-	# MD5 is different, so it's time to get busy!
-	printf "\nUpdated SSL certificate available. Proceeding with import...\n"
-	fi
+  # Check to see whether LE certificate has changed
+  printf "\nInspecting current SSL certificate...\n"
+  if md5sum -c "${LE_LIVE_DIR}/${UNIFI_HOSTNAME}/privkey.pem.md5" &>/dev/null; then
+    # MD5 remains unchanged, exit the script
+    printf "\nCertificate is unchanged, no update is necessary.\n"
+    exit 0
+  else
+    # MD5 is different, so it's time to get busy!
+    printf "\nUpdated SSL certificate available. Proceeding with import...\n"
+  fi
 fi
 
 # Verify required files exist
 if [[ ! -f ${PRIV_KEY} ]] || [[ ! -f ${CHAIN_FILE} ]]; then
-	printf "\nMissing one or more required files. Check your settings.\n"
-	exit 1
+  printf "\nMissing one or more required files. Check your settings.\n"
+  exit 1
 else
-	# Everything looks OK to proceed
-	printf "\nImporting the following files:\n"
-	printf "Private Key: %s\n" "$PRIV_KEY"
-	printf "CA File: %s\n" "$CHAIN_FILE"
+  # Everything looks OK to proceed
+  printf "\nImporting the following files:\n"
+  printf "Private Key: %s\n" "$PRIV_KEY"
+  printf "CA File: %s\n" "$CHAIN_FILE"
 fi
 
 # Create temp files
@@ -107,25 +107,25 @@ printf "\nStopping UniFi Controller...\n"
 service "${UNIFI_SERVICE}" stop
 
 if [[ ${LE_MODE} == "true" ]]; then
-	
-	# Write a new MD5 checksum based on the updated certificate	
-	printf "\nUpdating certificate MD5 checksum...\n"
+  
+  # Write a new MD5 checksum based on the updated certificate	
+  printf "\nUpdating certificate MD5 checksum...\n"
 
-	md5sum "${PRIV_KEY}" > "${LE_LIVE_DIR}/${UNIFI_HOSTNAME}/privkey.pem.md5"
-	
+  md5sum "${PRIV_KEY}" > "${LE_LIVE_DIR}/${UNIFI_HOSTNAME}/privkey.pem.md5"
+
 fi
 
 # Create double-safe keystore backup
 if [[ -s "${KEYSTORE}.orig" ]]; then
-	printf "\nBackup of original keystore exists!\n"
-	printf "\nCreating non-destructive backup as keystore.bak...\n"
-	cp "${KEYSTORE}" "${KEYSTORE}.bak"
+  printf "\nBackup of original keystore exists!\n"
+  printf "\nCreating non-destructive backup as keystore.bak...\n"
+  cp "${KEYSTORE}" "${KEYSTORE}.bak"
 else
-	cp "${KEYSTORE}" "${KEYSTORE}.orig"
-	printf "\nNo original keystore backup found.\n"
-	printf "\nCreating backup as keystore.orig...\n"
+  cp "${KEYSTORE}" "${KEYSTORE}.orig"
+  printf "\nNo original keystore backup found.\n"
+  printf "\nCreating backup as keystore.orig...\n"
 fi
-	 
+   
 # Export your existing SSL key, cert, and CA data to a PKCS12 file
 printf "\nExporting SSL certificate and key data into temporary PKCS12 file...\n"
 
@@ -144,11 +144,11 @@ else
     -out "${P12_TEMP}" -passout pass:"${PASSWORD}" \
     -name "${ALIAS}"
 fi
-	
+  
 # Delete the previous certificate data from keystore to avoid "already exists" message
 printf "\nRemoving previous certificate data from UniFi keystore...\n"
 keytool -delete -alias "${ALIAS}" -keystore "${KEYSTORE}" -deststorepass "${PASSWORD}"
-	
+  
 # Import the temp PKCS12 file into the UniFi keystore
 printf "\nImporting SSL certificate into UniFi keystore...\n"
 keytool -importkeystore \
@@ -162,7 +162,7 @@ keytool -importkeystore \
 # Clean up temp files
 printf "\nRemoving temporary files...\n"
 rm -f "${P12_TEMP}"
-	
+  
 # Restart the UniFi Controller to pick up the updated keystore
 printf "\nRestarting UniFi Controller to apply new Let's Encrypt SSL certificate...\n"
 service "${UNIFI_SERVICE}" start
