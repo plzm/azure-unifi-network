3f1cce700756c93589cf263e34569dfe8370b824
Patrick El-Azem <p@elazem.com>
2024-04-17 06:23:22 -0400

    Use apt-get instead of apt

--- scripts/controller/controller_2.sh
+++ scripts/controller/controller_2.sh
@@ -6,7 +6,7 @@ touch ~/controller_2.txt
 # TLS Certificate
 # Ensure DNS CNAME record already exists
 # sudo apt install certbot python3-certbot-apache -y
-sudo apt install snapd -y
+sudo apt-get install snapd -y
 sudo snap install --classic certbot
 sudo ln -sf /snap/bin/certbot /usr/bin/certbot
 
